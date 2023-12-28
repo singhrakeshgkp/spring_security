5fd2f44aaa793b0272ee21d413a8af983078d69f
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2023-12-27 17:39:58 -0800

    configured endpoint based authorization

--- security-basic/src/main/java/com/security/basic/SecurityBasicApplication.java
+++ security-basic/src/main/java/com/security/basic/SecurityBasicApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -21,6 +23,28 @@ public class SecurityBasicApplication {
 class SecurityController{
 	@GetMapping("/security-test")
 	public String test(){
+		var authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		authorities.forEach(authority -> System.out.println(authority.getAuthority()));
 		return "welcome to spring security";
 	}
+	@DeleteMapping("/delete")
+	public String delete(){
+		var authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    authorities.forEach(authority -> System.out.println(authority.getAuthority()));
+		return "performing delete operation";
+	}
+	@PostMapping("/write")
+	public String write(){
+		var authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		authorities.forEach(authority -> System.out.println(authority.getAuthority()));
+		return "performing write operation";
+	}
+
+	@GetMapping("/read")
+	public String read(){
+		var authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		authorities.forEach(authority -> System.out.println(authority.getAuthority()));
+		return "performing read operation";
+	}
 }
+
--- security-basic/src/main/java/com/security/basic/config/SecurityConfig.java
+++ security-basic/src/main/java/com/security/basic/config/SecurityConfig.java
@@ -1,28 +1,64 @@
 package com.security.basic.config;
 
-import com.security.basic.config.security.filter.ApiKeyFilter;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-  @Value("${secret.key}")
-  private String key;
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
     return httpSecurity
-        .addFilterBefore(new ApiKeyFilter(key), BasicAuthenticationFilter.class)
         .httpBasic(Customizer.withDefaults())
-        .authorizeHttpRequests(authorize->authorize.anyRequest()
-            .authenticated())
+        .csrf(AbstractHttpConfigurer::disable)// not recommended, if not disable from postman post req will not work
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                   .requestMatchers("write/**")
+                    .hasAnyRole("admin")
+                    .requestMatchers(HttpMethod.GET,"write/**")
+                    .hasAnyAuthority("ROLE_admin","write")
+                    .anyRequest()
+                    .authenticated())
         .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    var userDetailsService = new InMemoryUserDetailsManager();
+    var user1 =
+        User.builder()
+            .username("rakesh")
+            .password(passwordEncoder().encode("123"))
+            .authorities("write", "read","delete")
+            .roles("admin")
+            .build();
+    var user2 =
+        User.builder()
+            .username("suresh")
+            .password(passwordEncoder().encode("1234"))
+            .authorities("read")
+            .roles("user")
+            .build();
+    userDetailsService.createUser(user1);
+    userDetailsService.createUser(user2);
+    return userDetailsService;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
