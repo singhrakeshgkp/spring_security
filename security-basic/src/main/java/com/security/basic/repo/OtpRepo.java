5ea2971021772c90a6fa780b1028ba5511718ec0
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2026-08-17 09:24:33 -0600

    security basic

--- security-basic/src/main/java/com/security/basic/repo/OtpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.security.basic.repo;
-
-import com.security.basic.entity.Otp;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OtpRepo extends JpaRepository<Otp,Integer> {
-  Optional<Otp> getOtpByUserName(String userName);
-}
